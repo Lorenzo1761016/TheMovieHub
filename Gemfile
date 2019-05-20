0366f8bacf6565e00f65850643549935dc2e0a34
Davide <davimass97@gmail.com>
2019-05-20 11:04:19 +0200

    User 0.5
    User controller e view buildati
    1° 0 (perchè non release finale)
    2° num (5) (num del capitolo fatto nella guida)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+
+gem 'bootstrap-sass', '~>3.2.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root 'static_pages#home'
+
+  get 'users/new'
+  root             'static_pages#home'
+
+  get 'home'    => 'static_pages#home'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
