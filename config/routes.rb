988f1db5f955ea1de64884d176da0f95e43717b7
Davide <davimass97@gmail.com>
2019-05-25 18:39:30 +0200

    User 0.8.2.5
    Log in effettuato ma errori con i test, da vedere le funzioni

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
   root             'static_pages#home'
 
@@ -8,6 +9,9 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
