b39b9e0eb2a8a6ebd3c46a4703e5676732a10171
Davide <davimass97@gmail.com>
2019-06-04 11:59:22 +0200

    User 0.9.3
    Nuova pagina di edit + pagina per mostrare gli utenti

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,8 +8,7 @@ module SessionsHelper
     def current_user
       if (user_id = session[:user_id])
         @current_user ||= User.find_by(id: user_id)
-      elsif (user_id = cookies.signed[:user_id])
-        raise       # The tests still pass, so this branch is currently untested. 
+      elsif (user_id = cookies.signed[:user_id]) 
         user = User.find_by(id: user_id)
         if user && user.authenticated?(cookies[:remember_token])
           log_in user
@@ -39,4 +38,18 @@ module SessionsHelper
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
     end
+
+    def current_user?(user)
+      user == current_user
+    end
+
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+  
+    def store_location
+      session[:forwarding_url] = request.url if request.get?
+    end
+
   end
