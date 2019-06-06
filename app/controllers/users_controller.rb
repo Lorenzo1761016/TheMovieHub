6a14833b6c035bcf60147853fd0a044cae16ca9c
Davide <davimass97@gmail.com>
2019-06-06 11:47:27 +0200

    User 0.9.4
    Aggiunta admin e funzionalità (rimozione utenti)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def index
     @users = User.paginate(page: params[:page])
@@ -15,6 +16,16 @@ class UsersController < ApplicationController
   end
 
   def home
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def help
