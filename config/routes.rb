aa627756f1cfc9650b474e14a07dc75bfd8014ef
Davide <davimass97@gmail.com>
2019-06-08 10:31:08 +0200

    Revert "Aggiornamento master con la sezione user"
    This reverts commit 7a49873b83d614f662e79ace6576471aaac9cd90.

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,4 @@ Rails.application.routes.draw do
   resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tweets/index'
-  post 'tweets/show'
 end
