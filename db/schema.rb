662df96fdefb3024e6485cc6f6fd1eae01286c5a
Davide <davimass97@gmail.com>
2019-06-08 10:36:07 +0200

    Auto stash before revert of "Aggiornamento master con la sezione user"

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2019_06_06_091305) do
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
