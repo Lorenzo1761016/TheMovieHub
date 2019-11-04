cd84ae3d81e84206c2c599d1f460bc10bbaa5b95
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-11-04 18:39:38 +0100

    Film Front End 0.1

---
+++ app/controllers/film_controller.rb
@@ -0,0 +1,4 @@
+class FilmController < ApplicationController
+  def index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_064246) do
+ActiveRecord::Schema.define(version: 2019_10_04_104047) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "title"
