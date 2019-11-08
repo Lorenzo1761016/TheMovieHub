ea66014579e26299c5bc971904d67b8d1d56badf
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-11-08 19:24:21 +0100

    Film Back End 0.5

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -7,5 +7,11 @@ class FilmController < ApplicationController
   end
   def result
     @film = Tmdb::Movie.detail(params[:id])
+    @cast = Tmdb::Movie.cast(params[:id])
+    @video = Tmdb::Movie.videos(params[:id])
+    @keywords = Tmdb::Movie.keywords(params[:id])
+    @similar = Tmdb::Movie.similar(params[:id])
+
+
   end
 end
