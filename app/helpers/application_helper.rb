cae58569dc6df35121b92ec9597c96050805ee4a
antonio <plutoboy97@gmail.com>
2019-11-17 17:07:13 +0100

    Front-End fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
     # Returns the full title on a per-page basis.
     def full_title(page_title = '')
-      base_title = "Ruby on Rails Tutorial Sample App"
+      base_title = "Moviepedia"
       if page_title.empty?
         base_title
       else
-        "#{page_title} | #{base_title}"
+        "#{base_title} | #{page_title}"
       end
     end
   end
