6ae7ab4ad79837e44091cba5d6211aceced211da
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-12-10 12:36:19 +0100

    Admin 0.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+  before_action :require_admin
+ 
+
   def index
     @users = User.all
   end
   def show
+  end
+
+  private
+  def require_admin
+  unless current_user
+    flash[:error] =  "DEVI ESSERE ADMIN"
+      redirect_to new_user_session_path # halts request cycle
+    end
   end
 end
---
+++ db/migrate/20191210112513_add_ban_to_users.rb
@@ -0,0 +1,5 @@
+class AddBanToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :banned, :boolean
+  end
+end
