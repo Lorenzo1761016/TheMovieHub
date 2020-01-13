6eed9fac9667023b8a69be9c54474a8e15438173
SpartanDave097 <davimass97@gmail.com>
2020-01-13 18:13:49 +0100

    Piccolo update homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  
-  root             'static_pages#home'
+
+   root 'static_pages#home'
+          
   devise_for :users , controllers: {
     session: 'users/session', 
     registration: 'users/registration',
     omniauth: 'users/omniauth',
     omniauth_callbacks: "users/omniauth_callbacks",
-    confirmations: 'users/confirmations'}  
+    confirmations: 'users/confirmations'}
+
   get 'home'           => 'static_pages#home'
   get 'about'          => 'static_pages#about'
   get 'contact'        => 'static_pages#contact'
+  
   get 'chat'           => 'chat#index'
   get 'persona/:id', to: 'film#persona', as: 'persona'
   get 'tv/:id/season/:number/episode/:episode', to: 'film#episode', as: 'episode'
@@ -46,7 +49,9 @@ Rails.application.routes.draw do
       put "dislike" => "comments#dislike"
     end
   end
+
   resources :tweets
   resources :users, :only =>[:show, :index, :update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
