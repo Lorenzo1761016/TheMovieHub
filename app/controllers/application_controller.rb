381521c04cd7f40fb7f271e0794527469eaa4c2e
antonio <plutoboy97@gmail.com>
2020-01-20 18:48:06 +0100

    logIn fixato + altri fix vari
    iniziato a cambiare l'edit per utenti twitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
     before_action :configure_permitted_parameters, if: :devise_controller?
-   # after_action :track_action
+    after_action :track_action
 
 
   protected
