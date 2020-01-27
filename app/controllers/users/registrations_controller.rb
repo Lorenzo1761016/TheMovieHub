d7ec469ffa7a4d67a66765946088c2f2c3ffcc0b
antonio <plutoboy97@gmail.com>
2020-01-27 10:35:26 +0100

    go back

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,36 +18,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  # Overwrite update_resource to let users to update their user without giving their password
-  def update_resource(resource, params)
-    if resource.provider == "twitter"
-      params.delete("current_password")
-      resource.update_without_password(params)
-    else
-      resource.update_with_password(params)
-    end
-  end
-
   # PUT /resource
   def update
-    # For Rails 4
-    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
-
-    # required for settings form to submit when password is left blank
-    if account_update_params[:password].blank?
-      account_update_params.delete("password")
-      account_update_params.delete("password_confirmation")
-    end
-
-    @user = User.find(current_user.id)
-    if @user.update_attributes(account_update_params)
-      set_flash_message :notice, :updated
-      # Sign in the user bypassing validation in case their password changed
-      sign_in @user, :bypass => true
-      redirect_to after_update_path_for(@user)
-    else
-      render "/"
-    end
+    super
   end
 
   # DELETE /resource
