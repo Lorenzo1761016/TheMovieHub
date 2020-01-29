c717d96b2bac3fefd0ab4a7dd4f7a3186faf7c98
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2020-01-29 10:41:19 +0100

    Creazione Admin al lancio + Relazione pdf

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,9 @@ class StaticPagesController < ApplicationController
   end
 
   def home
+    if create_admin
+      puts("Admin creato!")
+    end
     ahoy.track "Homepage", language: "Ruby"
   end
 
@@ -18,6 +21,28 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  private
+
+  def create_admin
+    users = User.all
+    count =  0
+    users.each do |u|
+      if u.role =="A"
+        puts("Admin: "+u.username)
+        count = count+1
+      end
+    end
+    if count == 0
+      u = User.new(email: "lorenzo.scarlino.98@gmail.com", username: "LorenzoAdmin", first_name: "Lorenzo", last_name: "Admin", password: "oooooo", role: "A")
+      u.save!
+      puts( "Admin creato")
+      return true
+    else
+      puts("Ci sono già Admin")
+      return false
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,23 @@ module ApplicationHelper
       end
     end
 
+    def create_admin
+      users = User.All
+      count =  0
+      users.each do |u|
+        if u.role =="A"
+          puts("Admin: "+u.username)
+          count = count+1
+        end
+      end
+      if count == 0
+        u = User.new(email: "lorenzo.scarlino.98@gmail.com", first_name: "Lorenzo", last_name: "Admin", password: "oooooo", role: "A")
+        u.save!
+        puts( "Admin creato")
+        return true
+      else
+        puts("Ci sono già Admin")
+        return false
+      end
+    end
   end
